c828ac66bd86afc70e39d7095b27d8733a790776
lil-jon-crunk <git@playuw.com>
2026-02-25 04:54:52 +0300

    Fix build issues & remove slop

--- build.config.ts
+++ build.config.ts
@@ -1,19 +1,5 @@
 import { defineBuildConfig } from "unbuild";
 
 export default defineBuildConfig({
-  entries: [
-    {
-      name: "runtime",
-      input: "src/runtime",
-      outDir: "dist/runtime",
-      builder: "mkdist",
-      declaration: false,
-      pattern: [
-        "**",
-        "!**/*.stories.{js,cts,mts,ts,jsx,tsx}",
-        "!**/*.{spec,test}.{js,cts,mts,ts,jsx,tsx}",
-      ],
-    },
-  ],
-  declaration: false,
+  declaration: true,
 });
--- src/runtime/composables/public/useDynamicRouteParam.d.ts
+++
@@ -1,2 +0,0 @@
-export { useDynamicRouteParam } from '../extend/dynamicRoute/useDynamicRouteParam.js';
-export { useDynamicRoute } from '../extend/dynamicRoute/useDynamicRoute.js';
